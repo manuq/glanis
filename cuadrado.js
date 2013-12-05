6423ebf4c7d658aee51edf921aa6786e504bad6b
Manuel Quiñones <manuel.por.aca@gmail.com>
2013-12-05 02:43:31 -0300

    CSS renderer

--- cuadrado.js
+++ cuadrado.js
@@ -1,16 +1,19 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.CSS3DRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.CubeGeometry(1,1,0);
-var material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-var plane = new THREE.Mesh( geometry, material );
+var element = document.createElement( 'div' );
+element.style.width = '100px';
+element.style.height = '100px';
+element.style.background = "#ffff00";
+
+var plane = new THREE.CSS3DObject( element );
 scene.add( plane );
 
-camera.position.z = 5;
+camera.position.z = 350;
 
 function render() {
     plane.rotation.x += 0.1;
