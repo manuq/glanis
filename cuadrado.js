9fe753b50d80b3657bd7e96e5d8f07cfcfe7de36
Manuel Quiñones <manuel.por.aca@gmail.com>
2013-12-05 02:55:25 -0300

    small reorder

--- cuadrado.js
+++ cuadrado.js
@@ -1,5 +1,7 @@
 var scene = new THREE.Scene();
+
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 350;
 
 var renderer = new THREE.CSS3DRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -13,12 +15,12 @@ element.style.background = "#ffff00";
 var plane = new THREE.CSS3DObject( element );
 scene.add( plane );
 
-camera.position.z = 350;
-
 function render() {
+    requestAnimationFrame(render);
+
     plane.rotation.x += 0.1;
     plane.rotation.y += 0.01;
-    requestAnimationFrame(render);
+
     renderer.render(scene, camera);
 }
 render();
