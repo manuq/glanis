8c58b31dbb99703a9ead2818e98512cbf9869c58
Manuel Quiñones <manuel.por.aca@gmail.com>
2013-12-05 18:52:39 -0300

    Animated transitions

--- cuadrado.js
+++ cuadrado.js
@@ -34,47 +34,66 @@ function createFramesList(amount) {
 }
 
 
-function sequenceLayout() {
+function sequenceLayout(callback) {
     var curX = ((frameWidth * (frames.length - 0.5)) + (space * (frames.length - 1)))  / -2;
     for (var i=0; i<frames.length; i++) {
-        frames[i].position.x = curX;
-        frames[i].position.y = 0;
-        frames[i].position.z = 0;
-        frames[i].position.z = 0;
-        frames[i].element.style.opacity = 0.8;
+        var frame = frames[i];
+
+        var targetPosition = {x: curX, y: 0, z: 0};
+        var tweenPosition = new TWEEN.Tween(frame.position).to(targetPosition, 500);
+        tweenPosition.easing(TWEEN.Easing.Quadratic.InOut);
+        tweenPosition.start();
+
+        var targetStyle = {opacity: 0.8};
+        var tweenOpacity = new TWEEN.Tween(frame.element.style).to(targetStyle, 2000);
+        tweenOpacity.easing(TWEEN.Easing.Quadratic.InOut);
+        tweenOpacity.start();
 
         curX += frameWidth + space;
     };
 
-    camera.position.z = 350;
+    var targetCameraPosition = {x: 0, y: 0, z: 350};
+    var tweenCameraPosition = new TWEEN.Tween(camera.position).to(targetCameraPosition, 2500);
+    tweenCameraPosition.easing(TWEEN.Easing.Quadratic.InOut);
+    tweenCameraPosition.start().onComplete(callback);
 }
 
-function stackLayout() {
+function stackLayout(callback) {
     var curZ = 0;
     for (var i=0; i<frames.length; i++) {
-        frames[i].position.x = 0;
-        frames[i].position.y = 0;
-        frames[i].position.z = curZ;
-        frames[i].element.style.opacity = 0.2;
+        var frame = frames[i];
+
+        var targetPosition = {x: 0, y: 0, z: curZ};
+        var tweenPosition = new TWEEN.Tween(frame.position).to(targetPosition, 500);
+        tweenPosition.easing(TWEEN.Easing.Quadratic.InOut);
+        tweenPosition.start();
+
+        var targetStyle = {opacity: 0.2};
+        var tweenOpacity = new TWEEN.Tween(frame.element.style).to(targetStyle, 2000);
+        tweenOpacity.easing(TWEEN.Easing.Quadratic.InOut);
+        tweenOpacity.start();
 
         curZ -= space / 10;
     };
 
-    camera.position.z = 100;
+    var targetCameraPosition = {x: 0, y: 0, z: 100};
+    var tweenCameraPosition = new TWEEN.Tween(camera.position).to(targetCameraPosition, 2500);
+    tweenCameraPosition.easing(TWEEN.Easing.Quadratic.InOut);
+    tweenCameraPosition.start().onComplete(callback);
 }
 
 function render() {
     requestAnimationFrame(render);
 
-    // Update objects here
+    TWEEN.update();
 
     renderer.render(scene, camera);
 }
 
 function main() {
     frames = createFramesList(7);
-    sequenceLayout();
-//    stackLayout();
+//    sequenceLayout(function () {stackLayout()});
+    stackLayout(function () {sequenceLayout()});
     render();
 }
 
