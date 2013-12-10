8750fc5354e95027ba1d3d7799d8b9741cec6b12
Manuel Quiñones <manuel.por.aca@gmail.com>
2013-12-10 01:10:42 -0300

    Set shadow for each layout

--- cuadrado.js
+++ cuadrado.js
@@ -99,6 +99,10 @@ function sequenceLayout(callback) {
         curX += frameWidth + space;
     };
 
+    shadow.element.style.width = "3200px";
+    shadow.element.style.height = "300px";
+    shadow.element.style.opacity = 0.05;
+
     var targetCameraPosition = {x: 0, y: 0, z: 1000};
     var tweenCameraPosition = new TWEEN.Tween(camera.position).to(targetCameraPosition, 2500);
     currentTweens.push(tweenCameraPosition);
@@ -135,6 +139,10 @@ function stackLayout(callback) {
         curZ += space;
     };
 
+    shadow.element.style.width = "550px";
+    shadow.element.style.height = "800px";
+    shadow.element.style.opacity = 0.1;
+
     var targetCameraPosition = {x: 400, y: 250, z: 700};
     var tweenCameraPosition = new TWEEN.Tween(camera.position).to(targetCameraPosition, 2500);
     currentTweens.push(tweenCameraPosition);
@@ -170,6 +178,10 @@ function lightBoxLayout(callback) {
 
         curZ += space / 10;
     };
+
+    shadow.element.style.width = "500px";
+    shadow.element.style.height = "300px";
+    shadow.element.style.opacity = 0.1;
 
     var targetCameraPosition = {x: 0, y: 0, z: 300};
     var tweenCameraPosition = new TWEEN.Tween(camera.position).to(targetCameraPosition, 2500);
