d8b10cf0078071401e5c8ba8e0e3a7f0472dd257
Manuel Quiñones <manuel.por.aca@gmail.com>
2013-12-18 23:14:23 -0300

    Radios, set active and fix bugs

--- js/app/ui/radiobutton.js
+++ js/app/ui/radiobutton.js
@@ -3,23 +3,32 @@ define(["domReady!"], function(doc) {
     var RadioButton = function (name, options) {
         this.buttons = [];
         this.domElement = document.createElement('div');
+        var that = this;
 
-        for (var i=0; i<options.length; i++) {
-            var text = options[i]['text'];
-            var action = options[i]['action'];
+        options.forEach(function (opt, i) {
+            var text = opt['text'];
+            var action = opt['action'];
 
             var button = document.createElement('button');
-            this.domElement.appendChild(button);
-            this.buttons.push(button);
+            that.domElement.appendChild(button);
+            that.buttons.push(button);
             button.id = name + "-" + i;
             button.innerText = text;
-            button.onclick = action;
+            button.addEventListener('click',
+                                    function () { that.onButtonPress(button, action) });
 
             if (i == 0) {
                 button.classList.add('active');
             }
 
-        };
+        });
+    }
+
+    RadioButton.prototype.onButtonPress = function (button, action) {
+        action(function () {});
+        this.buttons.forEach(function (but) {
+            but.classList.toggle('active', but == button);
+        });
     }
 
     return RadioButton;
