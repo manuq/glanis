73664d07fcbe07b1137514ed8a3d29a78de12e25
Manuel Quiñones <manuel.por.aca@gmail.com>
2013-12-27 17:49:12 -0300

    UI layout

--- js/app/ui.js
+++ js/app/ui.js
@@ -6,6 +6,17 @@ function(doc,
     var ui = {};
     var domElement;
     var widgets = {};
+    var curRow = 0;
+
+    var setContainerClasses = function (elem) {
+        if (curRow == 0) {
+            elem.classList.add('first');
+        }
+        if (curRow % 2 !== 0) {
+            elem.classList.add('odd');
+        }
+        curRow += 1;
+    }
 
     ui.init = function () {
         domElement = document.createElement('div');
@@ -22,6 +33,7 @@ function(doc,
         elem = document.createElement('div');
         elem.className = "ui-container";
         domElement.appendChild(elem);
+        setContainerClasses(elem);
         var button = document.createElement('button');
         elem.appendChild(button);
         widgets[name] = button;
@@ -45,12 +57,14 @@ function(doc,
         name = options['name'];
         widgets[name] = pull;
         domElement.appendChild(pull.domElement);
+        setContainerClasses(pull.domElement);
     }
 
     ui.addRadioButtons = function (name, options) {
         var radio = new RadioButton(name, options);
         widgets[name] = radio;
         domElement.appendChild(radio.domElement);
+        setContainerClasses(radio.domElement);
     }
 
     ui.setRadioActive = function (radioName, butName) {
